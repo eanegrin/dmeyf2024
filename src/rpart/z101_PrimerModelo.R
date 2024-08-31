45f65304647acb89a6ccbe8657b36eed4cdf8a1c
eanegrin <eanegrin@gmail.com>
2024-08-31 16:28:38 -0300

    Revert "Codigo en tidyverse para generar la clase ternaria"
    This reverts commit a2d36e0aeaac6b08b3c2449c371f73c7e2ea9d4e.

--- src/rpart/z101_PrimerModelo.R
+++ src/rpart/z101_PrimerModelo.R
@@ -7,71 +7,13 @@ require("rpart")
 require("rpart.plot")
 
 # Aqui se debe poner la carpeta de la materia de SU computadora local
-setwd("C:/Eugenio/Maestria/DMEyF") # Establezco el Working Directory
+setwd("~/buckets/b1") # Establezco el Working Directory
 
 # cargo el dataset que tiene la clase calculada !
-dataset <- fread("./datasets/competencia_01_crudo.csv")
-
-# generar clase ternaria ----------------------------------------------------------------------------------
-
-# importo librerias adicionales
-
-library(dplyr)
-library(tidyr)
-library(lubridate)
-
-# convierto el periodo a tipo date y genero los 2 periodos subsiguientes para cada fila
-
-dataset$periodo0 <- as.Date(paste0(dataset$foto_mes,"01"),"%Y%m%d")
-
-dataset$periodo1 <- dataset$periodo0 %m+% months(1)
-dataset$periodo2 <- dataset$periodo1 %m+% months(1)
-
-# concateno el numero del cliente con los 3 periodos relevantes
-
-dataset$cliente_periodo0 <- paste(dataset$numero_de_cliente, dataset$periodo0, sep = " - ")
-dataset$cliente_periodo1 <- paste(dataset$numero_de_cliente, dataset$periodo1, sep = " - ")
-dataset$cliente_periodo2 <- paste(dataset$numero_de_cliente, dataset$periodo2, sep = " - ")
-
-# para el caso general, asigno baja+1 y baja+2 si la combinacion (id,periodo) no esta en la columna original
-
-dataset <- dataset %>% mutate(clase_ternaria = case_when(
-  !(cliente_periodo1 %in% dataset$cliente_periodo0) ~ "BAJA+1",
-  !(cliente_periodo2 %in% dataset$cliente_periodo0) ~ "BAJA+2",
-  .default = "CONTINUA"
-))
-
-# corrijo las asignaciones del paso anterior para los ultimos 2 meses
-
-ultimo_periodo <- max(dataset$periodo0)
-anteultimo_periodo <- ultimo_periodo %m-% months(1)
-
-dataset <- dataset %>% mutate(clase_ternaria = case_when(
-  (periodo0 == anteultimo_periodo) & (clase_ternaria != "BAJA+1") ~ NA,
-  periodo0 == ultimo_periodo ~ NA,
-  .default = clase_ternaria
-))
-
-# totales para cada periodo
-
-dataset %>%
-  group_by(periodo0, clase_ternaria) %>%
-  summarize(count = n()) %>%
-  arrange(periodo0, clase_ternaria) %>% 
-  pivot_wider(names_from = clase_ternaria, values_from = count)
-
-# elimino columnas auxiliares
-
-dataset <- dataset %>% select(-c(periodo0:cliente_periodo2))
-
-
-# ------------------------------------------------------------------------------------------------------
+dataset <- fread("./datasets/competencia_01.csv")
 
 dtrain <- dataset[foto_mes <= 202104] # defino donde voy a entrenar
 dapply <- dataset[foto_mes == 202106] # defino donde voy a aplicar el modelo
-
-unique(dtrain$foto_mes)
-
 
 # genero el modelo,  aqui se construye el arbol
 # quiero predecir clase_ternaria a partir de el resto de las variables
